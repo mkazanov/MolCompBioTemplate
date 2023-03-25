1c720565383bf4cb7130d32c51110172d7b87ca6
Marat Kazanov <mkazanov@gmail.com>
2023-03-25 16:52:24 +0300

    update dna

--- dna.cpp
+++ dna.cpp
@@ -8,11 +8,23 @@
 
 #include "dna.hpp"
 
+int CDNA::END_CHROMOSOME = -2;
+int CDNA::END_GENOME = -1;
+
 CDNAPos::CDNAPos(int chrNum_, unsigned long pos_)
 {
     chrNum = chrNum_;
     pos = pos_;
+    strand = "+";
 }
+
+CDNAPos::CDNAPos(int chrNum_, unsigned long pos_, string strand_)
+{
+    chrNum = chrNum_;
+    pos = pos_;
+    strand = strand_;
+}
+
 
 int CDNAPos::isNull()
 {
@@ -22,4 +34,34 @@ int CDNAPos::isNull()
         return(0);
 }
 
+CDNAInterval::CDNAInterval(int chrNum_, unsigned long startpos_, unsigned long endpos_)
+{
+    chrNum = chrNum_;
+    startpos = startpos_;
+    endpos = endpos_;
+    strand = "+";
+}
 
+CDNAInterval::CDNAInterval(int chrNum_, unsigned long startpos_, unsigned long endpos_, string strand_)
+{
+    chrNum = chrNum_;
+    startpos = startpos_;
+    endpos = endpos_;
+    strand = strand_;
+}
+
+int CDNAInterval::isNull()
+{
+    if(chrNum == -1)
+        return(1);
+    else
+        return(0);
+}
+
+int CDNAInterval::Check()
+{
+    if(!isNull() && startpos > endpos)
+        return(0);
+    else
+        return(1);
+}
--- dna.hpp
+++ dna.hpp
@@ -11,11 +11,14 @@
 
 #include <string>
 #include <algorithm>
+#include <map>
 
 using namespace std;
 
 class CDNA{
 public:
+    static int END_CHROMOSOME;
+    static int END_GENOME;
     static string cDNA(string dna)
     {
         string ret;
@@ -41,15 +44,70 @@ public:
         else
             return(false);
     }
+    static bool compareMotifDNA(string motif_, string seq_)
+    {
+        map<char,string> symbols;
+        char chs,chm;
+        string motif,seq;
+        
+        motif = motif_;
+        seq = seq_;
+        
+        if(motif.length() != seq.length())
+            return(false);
+        
+        transform(motif.begin(),motif.end(),motif.begin(),::toupper);
+        transform(seq.begin(),seq.end(),seq.begin(),::toupper);
+        
+        symbols['A'] = "A";
+        symbols['C'] = "C";
+        symbols['G'] = "G";
+        symbols['T'] = "T";
+        symbols['W'] = "ATW";
+        symbols['S'] = "CGS";
+        symbols['M'] = "ACM";
+        symbols['K'] = "GTK";
+        symbols['R'] = "AGR";
+        symbols['Y'] = "CTY";
+        symbols['B'] = "CGTSKYB";
+        symbols['D'] = "AGTRKWD";
+        symbols['H'] = "ACTMYWH";
+        symbols['V'] = "ACGMSRV";
+        symbols['N'] = "ACGTWSMKRYBDHVN";
+        
+        for(int i=0;i<motif.length();i++)
+        {
+            chm = motif[i];
+            chs = seq[i];
+            if (symbols[chm].find(chs) == std::string::npos)
+                return(false);
+        }
+        return(true);
+    }
 };
 
 class CDNAPos{
 public:
     int chrNum;
     unsigned long pos;
-    int strand;
-    CDNAPos(){};
+    string strand;
+    CDNAPos(){chrNum = -1;};
     CDNAPos(int chrNum_, unsigned long pos_);
+    CDNAPos(int chrNum_, unsigned long pos_, string strand_);
+    int isNull();
+};
+
+class CDNAInterval{
+public:
+    int chrNum;
+    unsigned long startpos;
+    unsigned long endpos;
+    string strand;
+    CDNAInterval(){};
+    CDNAInterval(int chrNum_);
+    CDNAInterval(int chrNum_, unsigned long startpos_, unsigned long endpos_);
+    CDNAInterval(int chrNum_, unsigned long startpos_, unsigned long endpos_, string strand_);
+    int Check();
     int isNull();
 };
 
